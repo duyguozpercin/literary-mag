1887b692b294daaf2dead7eac5ee61d45d8ebd25
duyguozpercin <duygucetind@gmail.com>
2025-07-29 23:54:11 +0300

    Add social icons to Header component and remove SarkacMotion component from Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import SarkacMotion from './components/SarkacMotion';
+
 
 export default function Home() {
   return (
     <div className="">
       
-      <main className="space-y-32 py-20">
-        
-       
-      </main>
+    
     </div>
   );
 }
