c0e4cb8aa24f4bb2989c9ce1d027654766f0d643
duyguozpercin <duygucetind@gmail.com>
2025-07-30 00:22:43 +0300

    Update Header component: adjust social icon sizes and integrate into Home and layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Header from './components/Header';
 
 
 export default function Home() {
   return (
     <div className="">
-      
+      <main>
+        
+      </main>
     
     </div>
   );
